c9df3747590b95b76ceb76c652cc29d6fe6beaf1
cllcnkrt <cllcnkrt@outlook.com>
2023-02-20 20:10:45 +0300

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import { Inter } from "@next/font/google";
 import "@/styles/reset.css";
 import "@/styles/global.css";
 
+const interFamily = Inter({ subsets: ["latin"] });
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={interFamily.className}>
       <head />
-      <body>{children}</body>
+      <body className="container">{children}</body>
     </html>
   );
 }
