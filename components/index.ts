cc3358d05ad23eeb44c9373eb437602b766fe781
cllcnkrt <cllcnkrt@outlook.com>
2023-04-06 15:15:35 +0300

    test 3

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,5 @@
-export { Categories } from "./categories";
 export { FeaturedMovie } from "./featured-movie";
 export { Header } from "./header";
 export { Loading } from "./loading";
+export { Categories } from "./categories";
 export { MoviesSection } from "./movies-section";
